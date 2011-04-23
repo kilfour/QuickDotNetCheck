df00bacb11c3525be04b4321220819bf04c63d2a
Mark Meyers <kilfour@gmail.com>
2011-04-23 14:42:02 +0200

    one that got away

---
+++ QuickDotNetCheck/ShrinkingStrategies/Get.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+
+namespace QuickDotNetCheck.ShrinkingStrategies
+{
+    public static class Get
+    {
+        public static Getter<T> From<T>(T target)
+        {
+            return new Getter<T>(target);
+        }
+    }
+
+    public class Getter<T>
+    {
+        private readonly T target;
+
+        public Getter(T target)
+        {
+            this.target = target;
+        }
+
+        public TProperty[] All<TProperty>()
+        {
+            var properties =
+                typeof(T).GetProperties()
+                    .Where(p => p.PropertyType == typeof(TProperty));
+            return
+                properties
+                    .Select(propertyInfo => (TProperty)propertyInfo.GetValue(target, null))
+                    .ToArray();
+        }
+    }
+}
