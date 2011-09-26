b14734d3f78f7d499dfc306afd7b279018065c2e
Mark Meyers <kilfour@gmail.com>
2011-09-26 09:22:43 +0200

    two that got away

---
+++ QuickDotNetCheck.ElaborateExample/People/Search/SearchPersonHandler.cs
@@ -0,0 +1,25 @@
+﻿using NHibernate;
+using NHibernate.Criterion;
+using QuickDotNetCheck.ElaborateExample.Domain;
+
+namespace QuickDotNetCheck.ElaborateExample.People.Search
+{
+    public class SearchPersonHandler
+    {
+        private readonly ISession session;
+
+        public SearchPersonHandler(ISession session)
+        {
+            this.session = session;
+        }
+
+        public Person Handle(SearchPersonRequest request)
+        {
+            return
+                session
+                    .CreateCriteria<Person>()
+                    .Add(Restrictions.Eq("FirstName", request.FirstName))
+                    .UniqueResult<Person>();
+        }
+    }
+}
---
+++ QuickDotNetCheck.ElaborateExample/People/Search/SearchPersonRequest.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace QuickDotNetCheck.ElaborateExample.People.Search
+{
+    public class SearchPersonRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //public string Title { get; set; }
+        //public DateTime BirthDate { get; set; }
+        //public string AddressStreet { get; set; }
+        //public string AddressCity { get; set; }
+        //public int AddressPostalCode { get; set; }
+        //public string AddressCountry { get; set; }
+        //public string Phone { get; set; }
+    }
+}
